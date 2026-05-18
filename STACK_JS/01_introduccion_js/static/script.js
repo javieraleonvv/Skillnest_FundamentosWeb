aff5fd6b0b2207c90434132ad9f147548a3a8d0c
mitsu-gami00 <javieraleon@liceovvh.cl>
2026-05-18 15:11:02 -0400

    ok mañlana

---
+++ STACK_JS/01_introduccion_js/static/script.js
@@ -0,0 +1,8 @@
+console.log("prueba de conexion js...")
+
+function cambiarTexto(){
+    let titulo = "hola, bievenido";
+    if(document.getElementById("titulo").innerText == titulo){
+        document.getElementById("titulo").innerText ==titulo
+    }
+}
